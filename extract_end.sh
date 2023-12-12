e900812cc4f3965a4edb07c8d531ad23d584722a
Ramneek <33387970+godfidence01@users.noreply.github.com>
2023-12-12 07:06:15 +0530

    Create extract_end.sh

---
+++ extract_end.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+# Check if jq is installed
+if ! command -v jq &> /dev/null; then
+    echo "jq is not installed. Please install it first."
+    exit 1
+fi
+
+# Check if a JSON file is provided as an argument
+if [ $# -eq 0 ]; then
+    echo "Usage: $0 <json_file>"
+    exit 1
+fi
+
+json_file=$1
+
+# Check if the file exists
+if [ ! -f "$json_file" ]; then
+    echo "Error: File $json_file not found."
+    exit 1
+fi
+
+# Use jq to extract URLs from the JSON file
+domain=$(jq -r '.. | .domain? // empty' "$json_file")
+
+# Print the extracted URLs
+echo "$domain"
